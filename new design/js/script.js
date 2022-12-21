6eac5432258cff9874d242d7eac595dccc207b78
rakeshcooper <rakeshmurali097@gmail.com>
2022-12-22 02:14:55 +0530

    Add files via upload

--- new design/js/script.js
+++ new design/js/script.js
@@ -1,32 +1,82 @@
 const fullname = document.getElementById('full_name')
+const email = document.getElementById('email')
 const workdetails = document.getElementById('workdetails')
 const subject = document.getElementById('subject')
 const form = document.getElementById('form')
 const error = document.getElementById('error')
+const active = [fullname,email,subject,workdetails]
 
 form.addEventListener('submit', (e) => { 
-
     let messages = []
-
-    if(subject.value === '' || subject.value == null){
-            messages.push("subject field is required!!!!")
-    }
-
-    else if(workdetails.value === '' || workdetails.value == null){
-        messages.push("workdetails field is required!!!!!!!!!!!!!")
-    }
-
-    else if(workdetails.value.length <= 50){
-        messages.push("workdetails must be atleast 50 words!!!!")
-    }
-
-    else if(workdetails.value.length >= 100){
-        messages.push("please enter work details less than 100 words!!!!")
-    }
-    
-
-    if(messages.length > 0){
-        e.preventDefault()
-        error.innerHTML = messages.join(',')
-    }
+    if(fullname.value === '' || fullname.value == null){
+         messages.push("Fullname field is required!!!!")
+         red(fullname);
+     }
+     else if(fullname.value.length > 0){
+         green(fullname);
+     }
+ 
+     
+ 
+     if(email.value === '' || email.value == null){
+         messages.push("email field is required!!!!")
+         red(email);
+     }
+     else if(email.value.length > 0){
+         green(email);
+     }
+ 
+     
+     
+     if(subject.value === '' || subject.value == null){
+             messages.push("subject field is required!!!!")
+             red(subject);
+     }
+     else if(subject.value.length > 0){
+         green(subject);
+     }
+ 
+ 
+     if(workdetails.value === '' || workdetails.value == null){
+         messages.push("workdetails field is required ")
+         red(workdetails);
+     }
+     else if(workdetails.value.length <= 50){
+         messages.push("workdetails must be atleast 50 words!!!!")
+         red(workdetails);
+     }
+     else if(workdetails.value.length >= 100){
+         messages.push("please enter work details less than 100 words!!!!")
+         red(workdetails);
+     }
+     else if(workdetails.value.length > 0){
+         green(workdetails);
+     }
+ 
+      
+     if(messages.length > 0){
+         e.preventDefault()
+         error.innerHTML = messages.join('<br>,')
+         error.style.display = "block";
+         
+     }
 })
+
+function red(color){
+    color.style.backgroundColor = "rgba(251, 196, 196, 0.65)";
+    color.style.border = "1px solid red";
+}
+
+function green(color){
+    color.style.backgroundColor = "";
+    color.style.border = "1px solid green";
+}
+
+
+/*function nored(){
+    active.forEach((value) => {
+        value.style.backgroundColor = ""
+        value.style.border = "";
+    })
+}*/
+
